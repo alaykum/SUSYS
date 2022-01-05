9d8f35974e7679539e498d84234413403b879c03
alaykum <96905615+alaykum@users.noreply.github.com>
2022-01-05 09:51:31 +0500

    initial commit

---
+++ leads/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from . import models
+
+def leads_lists(request):
+    leads = models.Lead.objects.all()
+    context = {
+        "leads" : leads
+    }
+
+    return render (request, "leads_lists.html", context)
